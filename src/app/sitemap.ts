082e2b0f1c27d275a7f8d424539c68ba563d9790
Lekens <arokoyuolalekan@gmail.com>
2026-06-05 18:31:34 +0100

    fix: first deployment

--- src/app/sitemap.ts
+++
@@ -1,7 +0,0 @@
-import { MetadataRoute } from 'next'
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    { url: 'https://poschoice.ng', lastModified: new Date(), changeFrequency: 'weekly', priority: 1 },
-  ]
-}
